0a8e2481929c209501a69c959af5ce20ca803be4
Jeremy Huffman <jeremy@jeremyhuffman.com>
2018-07-04 19:25:14 -0400

    Use cabal custom build to build duktape sources.

--- dukconfig/build_dist.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p dukconfig/dist
-
-python duktape/tools/configure.py \
-       --output-directory dukconfig/dist \
-       --source-directory duktape/src-input \
-       --config-metadata duktape/config \
-       --option-file dukconfig/config.yaml \
