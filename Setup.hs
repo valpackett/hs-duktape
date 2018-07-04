456f582c218ca1ecfaacdd03e13c334bbb6059d5
Jeremy Huffman <jeremy@jeremyhuffman.com>
2018-07-04 19:34:47 -0400

    unicode syntax

--- Setup.hs
+++ Setup.hs
@@ -1,5 +1,5 @@
 {-# OPTIONS_GHC -fno-warn-missing-signatures #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE UnicodeSyntax, LambdaCase #-}
 
 import           Data.List                          (isPrefixOf)
 import           Distribution.Simple
@@ -23,10 +23,10 @@ repoSource = "duktape/src/duktape.c"
 builtSource = "dukconfig/dist/duktape.c"
 
 buildDuktape _ _ = do
-  script <- doesFileExist scriptFile
+  script ← doesFileExist scriptFile
   if script then do
     createDirectoryIfMissing True outputDir
-    needBuild <- filesCurrent
+    needBuild ← filesCurrent
     if needBuild then do
       runScript
       done
@@ -35,45 +35,45 @@ buildDuktape _ _ = do
   where
   done = return emptyHookedBuildInfo
   filesCurrent = do
-    builtExists <- doesFileExist builtSource
+    builtExists ← doesFileExist builtSource
     if builtExists then do
-        repoTime <- getModificationTime repoSource
-        builtTime <- getModificationTime builtSource
+        repoTime ← getModificationTime repoSource
+        builtTime ← getModificationTime builtSource
         return $ repoTime > builtTime
     else return True
   runScript =
     findPython >>= \case
-        Right python ->
+        Right python →
           callProcess python (scriptFile : scriptArgs)
-        Left reason -> do
+        Left reason → do
             putStrLn "Couldn't find Python 2 with PyYAML, which is required to configure duktape."
             putStrLn reason
 
 -- Duketape's configure script requires python2 with PyYAML installed.
 -- Check candidates "python2, python" and return one that will work or reasons why
-findPython :: IO (Either String FilePath)
+findPython ∷ IO (Either String FilePath)
 findPython =
-    tryPython "python2" >>= \case
-       Left reason2 ->
-           tryPython "python" >>= \case
-               Left reason ->
-                   return $ Left $ concat ["python2: ", reason2, "\npython: ", reason]
-               found -> return found
-       found -> return found
+  tryPython "python2" >>= \case
+      Left reason2 →
+          tryPython "python" >>= \case
+              Left reason →
+                  return $ Left $ concat ["python2: ", reason2, "\npython: ", reason]
+              found → return found
+      found → return found
   where
-    tryPython exe = findExecutable exe >>= \case
-                      Nothing -> return $ Left "Executable not found."
-                      Just path -> checkPython path
-    checkPython path =
-       readProcessWithExitCode path ["--version"] "" >>= \case
-           (ExitSuccess, "", version) -> checkPrefix version path
-           (ExitSuccess, version, "") -> checkPrefix version path
-           (ExitFailure _, out, err) -> return $ Left (out ++ err)
-           _ -> return $ Left "Execution failed."
-    checkPrefix version path
+  tryPython exe = findExecutable exe >>= \case
+                      Nothing → return $ Left "Executable not found."
+                      Just path → checkPython path
+  checkPython path =
+      readProcessWithExitCode path ["--version"] "" >>= \case
+          (ExitSuccess, "", version) → checkPrefix version path
+          (ExitSuccess, version, "") → checkPrefix version path
+          (ExitFailure _, out, err) → return $ Left (out ++ err)
+          _ → return $ Left "Execution failed."
+  checkPrefix version path
               | "Python 2" `isPrefixOf` version = checkPyYAML path
               | otherwise = return $ Left version
-    checkPyYAML path =
-       readProcessWithExitCode path ["-c", "import yaml"] "" >>= \case
-           (ExitSuccess, "", _) -> return $ Right path
-           _ -> return $ Left "No PyYaml installed."
+  checkPyYAML path =
+      readProcessWithExitCode path ["-c", "import yaml"] "" >>= \case
+          (ExitSuccess, "", _) → return $ Right path
+          _ → return $ Left "No PyYaml installed."
